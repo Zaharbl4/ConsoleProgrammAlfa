38228558cd72f9cb7bfc77cab613903e198cc7a0
Zaharbl4 <103624541+Zaharbl4@users.noreply.github.com>
2022-05-10 15:44:02 +0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProgram
+{
+    internal class Class1
+    {
+        public static void Read()
+        {
+            Console.Write("Введите число ");
+            
+            
+        
+        }
+        public static int[] Array(int size)
+        {
+            
+            int[] array = new int[size];
+           
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = i;
+            }
+            return array;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleProgram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleprogram
+{
+    public class Program 
+    {
+        public static void Main()
+        {
+            Class1.Read();
+            Class1.Array(Convert.ToInt32(Console.ReadLine()));
+
+        }
+
+           
+    }
+}
